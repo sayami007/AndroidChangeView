f6255650dc4cf88e83b31f8b4e3c4bdb90eb83b7
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-10-30 14:44:01 +0545

    New Branch merojob created

--- app/src/main/java/manandharbibesh/viewswitch/MainActivity.java
+++ app/src/main/java/manandharbibesh/viewswitch/MainActivity.java
@@ -1,5 +1,6 @@
 package manandharbibesh.viewswitch;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,18 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ViewSwitcher switcher = (ViewSwitcher) findViewById(R.id.my_switcher);
-        final TextView myTV = (TextView) switcher.findViewById(R.id.clickable_text_view);
-        final EditText myET = (EditText) switcher.findViewById(R.id.hidden_edit_view);
-        final Button change = (Button) findViewById(R.id.change);
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switcher.showNext();
-                myTV.setText(myET.getText().toString());
-                myET.setText(myTV.getText().toString());
-            }
-        });
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Job Preferences");
     }
 
 
